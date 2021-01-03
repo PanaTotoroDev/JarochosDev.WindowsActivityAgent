acd51a2c7c04a98e90ffd6edd98c25bc1e37b8f3
Fernando David Villafuerte Gertrudis <fernandodavid582@outlook.com>
2021-01-03 13:33:44 -0600

    FVILL-Testing changes

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JarochosDev.WindowsActivityAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing changes");
+            Console.ReadLine();
+        }
+    }
+}
