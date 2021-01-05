63246afb04760fa49ed1b2afac2b876ff27ace72
Fernando David Villafuerte Gertrudis <fernandodavid582@outlook.com>
2021-01-05 15:23:46 -0600

    FVILL-This is a example of how to get my username with a c# code

--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ namespace JarochosDev.WindowsActivityAgent
 {
     class Program
     {
+        //This is a example of how to get my username with a c# code 
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing changes");
+            Console.WriteLine();
+            //  <-- Keep this information secure! -->
+            Console.WriteLine("UserName: {0}", Environment.UserName);
             Console.ReadLine();
         }
     }
